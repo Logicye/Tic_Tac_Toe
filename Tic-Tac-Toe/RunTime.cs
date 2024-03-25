3c8989713bd4212758b29c93bd3c06c19a7551a2
Logicye <93573184+Logicye@users.noreply.github.com>
2024-03-25 14:23:54 +1000

    Added nuget dependencies and implementation

--- Tic-Tac-Toe/RunTime.cs
+++ Tic-Tac-Toe/RunTime.cs
@@ -1,13 +1,38 @@
-﻿using Tic_Tac_Toe.Menu;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.IO;
 
 namespace Tic_Tac_Toe
 {
-    internal class RunTime
+    class RunTime
     {
         static void Main()
         {
+            /*
+            InputEventHandler inputEventHandler = new InputEventHandler();
+            Thread InputHandler = new Thread(inputEventHandler.KeyPressedCheck);
+            InputHandler.Start();
             Menus.MainMenu();
-            Console.ReadKey();
+            Console.ReadKey(true);
+            */
+
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .CreateLogger();
+
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Produciton"}.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
-}
+}  
