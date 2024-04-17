74a911f2edebefe9441c06ed2b7f116bc4954acc
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-17 13:30:46 +1000

    Added added new console key event methods

---
+++ Tic-Tac-Toe/Handlers/ConsoleKeyboardEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestingBase
+{
+	static class ConsoleKeyboardEventHandler
+	{
+		public delegate void KeyboardEvent(ConsoleKey key);
+		public static event KeyboardEvent? KeyPressed;
+
+		private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+		public static void Start()
+		{
+			_cancellationTokenSource = new CancellationTokenSource();		
+
+			while (!_cancellationTokenSource.IsCancellationRequested)
+			{
+				ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+				KeyPressed?.Invoke(keyInfo.Key);
+			}
+			Debug.WriteLine("Event Handler Finished Execution");
+		}
+
+		public static void Stop()
+		{
+			_cancellationTokenSource.Cancel();
+		}
+	}
+}
