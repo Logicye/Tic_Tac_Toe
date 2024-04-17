df8d9740249e47f684488efa312e4afeee5b9053
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-18 09:42:40 +1000

    Added a new ConsoleGameFramework Package - All future standardized console code will be added here for later use in future projects

---
+++ ConsoleGameFramework/ColorSpace/Color.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleGameFramework.ColorSpace
+{
+    public struct Color
+    {
+        public ConsoleColor ReferenceColor { get; }
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+
+        public Color(ConsoleColor color, int r, int g, int b)
+        {
+            ReferenceColor = color;
+            Red = r;
+            Green = g;
+            Blue = b;
+        }
+        public Color ReadableColor()
+        {
+            return FindFurtherestColor(Red, Green, Blue);
+            /*
+            if (ToArray().Sum() < (255 * 3) / 2)
+            {
+                return Colors.White;
+            }
+            return Colors.Black;
+			*/
+        }
+
+        public Color Inverse()
+        {
+            return FindNearestColor(255 - Red, 255 - Green, 255 - Blue);
+        }
+
+
+
+        private static Color FindNearestColor(int r, int g, int b)
+        {
+            Color furtherest = Colors.White;
+            int dif = 255 * 3;
+            foreach (Color color in Colors.AllColors())
+            {
+                int dist = Convert.ToInt32(Math.Sqrt(
+                    0.3 * Math.Pow(r - color.Red, 2) +
+                    0.59 * Math.Pow(g - color.Green, 2) +
+                    0.11 * Math.Pow(b - color.Blue, 2)));
+
+                if (dist < dif)
+                {
+                    dif = dist;
+                    furtherest = color;
+                }
+            }
+            return furtherest;
+        }
+
+        private static Color FindFurtherestColor(int r, int g, int b)
+        {
+            Color furtherest = Colors.White;
+            int dif = 0;
+            foreach (Color color in Colors.AllColors())
+            {
+                int dist = Convert.ToInt32(Math.Sqrt(
+                    0.3 * Math.Pow(r - color.Red, 2) +
+                    0.59 * Math.Pow(g - color.Green, 2) +
+                    0.11 * Math.Pow(b - color.Blue, 2)));
+
+                if (dist > dif)
+                {
+                    dif = dist;
+                    furtherest = color;
+                }
+            }
+            return furtherest;
+        }
+
+        public static Color operator -(Color color1, Color color2)
+        {
+            return FindNearestColor(
+                Convert.ToInt32(Math.Sqrt(color1.Red * color2.Red)),
+                Convert.ToInt32(Math.Sqrt(color1.Green * color2.Green)),
+                Convert.ToInt32(Math.Sqrt(color1.Blue * color2.Blue)));
+        }
+
+        public static Color operator +(Color color1, Color color2)
+        {
+            return FindFurtherestColor(
+                (color1.Red + color2.Red) / 2,
+                (color1.Green + color2.Green) / 2,
+                (color1.Blue + color2.Blue) / 2);
+        }
+
+        public readonly int[] ToArray() => new int[3] { Red, Green, Blue };
+
+    }
+}
---
+++ ConsoleGameFramework/ColorSpace/Colors.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace ConsoleGameFramework.ColorSpace
+{
+    public static class Colors
+    {
+        public static Color White { get; } = new Color(ConsoleColor.White, 242, 242, 242);
+        public static Color Black { get; } = new Color(ConsoleColor.Black, 12, 12, 12);
+        public static Color Blue { get; } = new Color(ConsoleColor.Blue, 59, 120, 255);
+        public static Color DarkBlue { get; } = new Color(ConsoleColor.DarkBlue, 0, 55, 218);
+        public static Color Green { get; } = new Color(ConsoleColor.Green, 22, 198, 12);
+        public static Color DarkGreen { get; } = new Color(ConsoleColor.DarkGreen, 19, 161, 14);
+        public static Color Cyan { get; } = new Color(ConsoleColor.Cyan, 97, 214, 214);
+        public static Color DarkCyan { get; } = new Color(ConsoleColor.DarkCyan, 58, 150, 221);
+        public static Color Red { get; } = new Color(ConsoleColor.Red, 231, 72, 86);
+        public static Color DarkRed { get; } = new Color(ConsoleColor.DarkRed, 197, 15, 31);
+        public static Color Magenta { get; } = new Color(ConsoleColor.Magenta, 180, 0, 158);
+        public static Color DarkMagenta { get; } = new Color(ConsoleColor.DarkMagenta, 136, 23, 152);
+        public static Color Yellow { get; } = new Color(ConsoleColor.Yellow, 249, 241, 165);
+        public static Color DarkYellow { get; } = new Color(ConsoleColor.DarkYellow, 193, 156, 0);
+        public static Color Gray { get; } = new Color(ConsoleColor.Gray, 204, 204, 204);
+        public static Color DarkGray { get; } = new Color(ConsoleColor.DarkGray, 118, 118, 118);
+
+        public static IEnumerable<Color> AllColors()
+        {
+            yield return White;
+            yield return Black;
+            yield return Blue;
+            yield return DarkBlue;
+            yield return Green;
+            yield return DarkGreen;
+            yield return Cyan;
+            yield return DarkCyan;
+            yield return Red;
+            yield return DarkRed;
+            yield return Magenta;
+            yield return DarkMagenta;
+            yield return Yellow;
+            yield return DarkYellow;
+            yield return Gray;
+            yield return DarkGray;
+
+        }
+    }
+}
