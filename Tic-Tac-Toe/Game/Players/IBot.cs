261f14980026536c3efdc0a2a9b51fe6712203b4
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-21 04:34:50 +1000

    Moved IBot interface and added primary funtionality

--- Tic-Tac-Toe/Game/Players/IBot.cs
+++ Tic-Tac-Toe/Game/Players/IBot.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tic_Tac_Toe.Games.Players.Bots
+namespace Tic_Tac_Toe.Game.Players
 {
     public interface IBot
     {
-    }
+		public Vector2 GetBestMove();
+		public void PathFind();
+		public void PathExecute();
+
+	}
 }
