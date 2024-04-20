0cceb870a9548029bf03a66de65c3be7f0e55b7b
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-20 11:13:21 +1000

    Added statndardized vectors (partial functionality, only has basic properties)

---
+++ ConsoleGameFramework/Objects/Vectors/Vector2.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleGameFramework.Objects.Vectors
+{
+    public struct Vector2 : IEquatable<Vector2>
+    {
+        #region Properties
+        public float X { get; }
+        public float Y { get; }
+        #endregion
+
+        #region Constructors
+        public Vector2(float x, float y) { X = x; Y = y; }
+        public Vector2(Vector3 v) { X = v.X; Y = v.Y; }
+        public Vector2(Vector4 v) { X = v.X; Y = v.Y; }
+        #endregion
+
+        #region Internal Function
+
+        #endregion
+
+        #region Standard Vector2's
+        public static Vector2 Zero() => new Vector2(0, 0);
+        public static Vector2 Left() => new Vector2(-1, 0);
+        public static Vector2 Right() => new Vector2(1, 0);
+        public static Vector2 Up() => new Vector2(0, 1);
+        public static Vector2 Down() => new Vector2(0, -1);
+        #endregion
+
+        #region Operator Logic
+        public bool Equals(Vector2 other) => X == other.X && Y == other.Y ? true : false;
+        public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+        public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+        public static Vector2 operator *(Vector2 v1, Vector2 v2) => new Vector2(v1.X * v2.X, v1.Y * v2.Y);
+        public static Vector2 operator /(Vector2 v1, Vector2 v2) => new Vector2(v1.X / v2.X, v1.Y / v2.Y);
+        public static bool operator ==(Vector2 v1, Vector2 v2) => v1.Equals(v2);
+        public static bool operator !=(Vector2 v1, Vector2 v2) => !v1.Equals(v2);
+        #endregion
+
+        #region Standard Methods
+        public override readonly string ToString() => $"( X: {X}, Y: {Y} )";
+        #endregion
+    }
+}
---
+++ ConsoleGameFramework/Objects/Vectors/Vector3.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleGameFramework.Objects.Vectors
+{
+    public struct Vector3 : IEquatable<Vector3>
+    {
+        #region Properties
+        public float X { get; }
+        public float Y { get; }
+        public float Z { get; }
+        #endregion
+
+        #region Constructors
+        public Vector3(float x, float y, float z) { X = x; Y = y; Z = z; }
+        public Vector3(Vector2 v, float z) { X = v.X; Y = v.Y; Z = z; }
+        public Vector3(Vector4 v) { X = v.X; Y = v.Y; Z = v.Z; }
+
+        #endregion
+
+
+        #region Internal Function
+
+        #endregion
+
+        #region Standard Vector3's
+        public static Vector3 Zero() => new Vector3(0, 0, 0);
+        public static Vector3 Left() => new Vector3(-1, 0, 0);
+        public static Vector3 Right() => new Vector3(1, 0, 0);
+        public static Vector3 Up() => new Vector3(0, 1, 0);
+        public static Vector3 Down() => new Vector3(0, -1, 0);
+        #endregion
+
+        #region Operator Logic
+        public bool Equals(Vector3 other) => X == other.X && Y == other.Y && Z == other.Z ? true : false;
+        public static Vector3 operator +(Vector3 v1, Vector3 v2) => new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        public static Vector3 operator -(Vector3 v1, Vector3 v2) => new Vector3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        public static Vector3 operator *(Vector3 v1, Vector3 v2) => new Vector3(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
+        public static Vector3 operator /(Vector3 v1, Vector3 v2) => new Vector3(v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z);
+        public static bool operator ==(Vector3 v1, Vector3 v2) => v1.Equals(v2);
+        public static bool operator !=(Vector3 v1, Vector3 v2) => !v1.Equals(v2);
+        #endregion
+
+        #region Standard Methods
+        public override readonly string ToString() => $"( X: {X}, Y: {Y}, Z: {Z} )";
+        #endregion
+
+    }
+}
