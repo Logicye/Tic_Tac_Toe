89a6abfc2d4e48172b1d7af08d45d221b9adc2cf
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-20 12:00:31 +1000

    added hash codes overrides to vectors and fixed vector4's eqauls bug

--- ConsoleGameFramework/Objects/Vectors/Vector2.cs
+++ ConsoleGameFramework/Objects/Vectors/Vector2.cs
@@ -1,42 +1,43 @@
 ﻿namespace ConsoleGameFramework.Objects.Vectors
 {
-    public struct Vector2 : IEquatable<Vector2>
-    {
-        #region Properties
-        public float X { get; }
-        public float Y { get; }
-        #endregion
+	public struct Vector2 : IEquatable<Vector2>
+	{
+		#region Properties
+		public float X { get; }
+		public float Y { get; }
+		#endregion
 
-        #region Constructors
-        public Vector2(float x, float y) { X = x; Y = y; }
-        public Vector2(Vector3 v) { X = v.X; Y = v.Y; }
-        public Vector2(Vector4 v) { X = v.X; Y = v.Y; }
-        #endregion
+		#region Constructors
+		public Vector2(float x, float y) { X = x; Y = y; }
+		public Vector2(Vector3 v) { X = v.X; Y = v.Y; }
+		public Vector2(Vector4 v) { X = v.X; Y = v.Y; }
+		#endregion
 
-        #region Internal Function
+		#region Internal Function
 
-        #endregion
+		#endregion
 
-        #region Standard Vector2's
-        public static Vector2 Zero() => new Vector2(0, 0);
-        public static Vector2 Left() => new Vector2(-1, 0);
-        public static Vector2 Right() => new Vector2(1, 0);
-        public static Vector2 Up() => new Vector2(0, 1);
-        public static Vector2 Down() => new Vector2(0, -1);
-        #endregion
+		#region Standard Vector2's
+		public static Vector2 Zero() => new(0, 0);
+		public static Vector2 Left() => new(-1, 0);
+		public static Vector2 Right() => new(1, 0);
+		public static Vector2 Up() => new(0, 1);
+		public static Vector2 Down() => new(0, -1);
+		#endregion
 
-        #region Operator Logic
-        public bool Equals(Vector2 other) => X == other.X && Y == other.Y ? true : false;
-        public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.X + v2.X, v1.Y + v2.Y);
-        public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.X - v2.X, v1.Y - v2.Y);
-        public static Vector2 operator *(Vector2 v1, Vector2 v2) => new Vector2(v1.X * v2.X, v1.Y * v2.Y);
-        public static Vector2 operator /(Vector2 v1, Vector2 v2) => new Vector2(v1.X / v2.X, v1.Y / v2.Y);
-        public static bool operator ==(Vector2 v1, Vector2 v2) => v1.Equals(v2);
-        public static bool operator !=(Vector2 v1, Vector2 v2) => !v1.Equals(v2);
-        #endregion
+		#region Operator Logic
+		public static Vector2 operator +(Vector2 v1, Vector2 v2) => new(v1.X + v2.X, v1.Y + v2.Y);
+		public static Vector2 operator -(Vector2 v1, Vector2 v2) => new(v1.X - v2.X, v1.Y - v2.Y);
+		public static Vector2 operator *(Vector2 v1, Vector2 v2) => new(v1.X * v2.X, v1.Y * v2.Y);
+		public static Vector2 operator /(Vector2 v1, Vector2 v2) => new(v1.X / v2.X, v1.Y / v2.Y);
+		public static bool operator ==(Vector2 v1, Vector2 v2) => v1.Equals(v2);
+		public static bool operator !=(Vector2 v1, Vector2 v2) => !v1.Equals(v2);
+		#endregion
 
-        #region Standard Methods
-        public override readonly string ToString() => $"( X: {X}, Y: {Y} )";
-        #endregion
-    }
+		#region Standard Methods
+		public override readonly string ToString() => $"( X: {X}, Y: {Y} )";
+		public readonly bool Equals(Vector2 other) => X == other.X && Y == other.Y;
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
+		#endregion
+	}
 }
--- ConsoleGameFramework/Objects/Vectors/Vector4.cs
+++ ConsoleGameFramework/Objects/Vectors/Vector4.cs
@@ -1,44 +1,45 @@
 ﻿namespace ConsoleGameFramework.Objects.Vectors
 {
-    public struct Vector4 : IEquatable<Vector4>
-    {
-        #region Properties
-        public float X { get; }
-        public float Y { get; }
-        public float Z { get; }
-        public float W { get; }
-        #endregion
+	public struct Vector4 : IEquatable<Vector4>
+	{
+		#region Properties
+		public float X { get; }
+		public float Y { get; }
+		public float Z { get; }
+		public float W { get; }
+		#endregion
 
-        #region Constructors
-        public Vector4(float x, float y, float z, float w) { X = x; Y = y; Z = z; W = w; }
-        public Vector4(Vector2 v, float z, float w) { X = v.X; Y = v.Y; Z = z; W = w; }
-        public Vector4(Vector3 v, float w) { X = v.X; Y = v.Y; Z = v.Z; W = w; }
-        #endregion
+		#region Constructors
+		public Vector4(float x, float y, float z, float w) { X = x; Y = y; Z = z; W = w; }
+		public Vector4(Vector2 v, float z, float w) { X = v.X; Y = v.Y; Z = z; W = w; }
+		public Vector4(Vector3 v, float w) { X = v.X; Y = v.Y; Z = v.Z; W = w; }
+		#endregion
 
-        #region Internal Function
+		#region Internal Function
 
-        #endregion
+		#endregion
 
-        #region Standard Vector3's
-        public static Vector4 Zero() => new Vector4(0, 0, 0, 0);
-        public static Vector4 Left() => new Vector4(-1, 0, 0, 0);
-        public static Vector4 Right() => new Vector4(1, 0, 0, 0);
-        public static Vector4 Up() => new Vector4(0, 1, 0, 0);
-        public static Vector4 Down() => new Vector4(0, -1, 0, 0);
-        #endregion
+		#region Standard Vector3's
+		public static Vector4 Zero() => new(0, 0, 0, 0);
+		public static Vector4 Left() => new(-1, 0, 0, 0);
+		public static Vector4 Right() => new(1, 0, 0, 0);
+		public static Vector4 Up() => new(0, 1, 0, 0);
+		public static Vector4 Down() => new(0, -1, 0, 0);
+		#endregion
 
-        #region Operator Logic
-        public bool Equals(Vector4 other) => X == other.X && Y == other.Y && Z == other.Z ? true : false;
-        public static Vector4 operator +(Vector4 v1, Vector4 v2) => new Vector4(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z, v1.W + v2.W);
-        public static Vector4 operator -(Vector4 v1, Vector4 v2) => new Vector4(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z, v1.W - v2.W);
-        public static Vector4 operator *(Vector4 v1, Vector4 v2) => new Vector4(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z, v1.W * v2.W);
-        public static Vector4 operator /(Vector4 v1, Vector4 v2) => new Vector4(v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z, v1.W / v2.W);
-        public static bool operator ==(Vector4 v1, Vector4 v2) => v1.Equals(v2);
-        public static bool operator !=(Vector4 v1, Vector4 v2) => !v1.Equals(v2);
-        #endregion
+		#region Operator Logic
+		public static Vector4 operator +(Vector4 v1, Vector4 v2) => new(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z, v1.W + v2.W);
+		public static Vector4 operator -(Vector4 v1, Vector4 v2) => new(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z, v1.W - v2.W);
+		public static Vector4 operator *(Vector4 v1, Vector4 v2) => new(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z, v1.W * v2.W);
+		public static Vector4 operator /(Vector4 v1, Vector4 v2) => new(v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z, v1.W / v2.W);
+		public static bool operator ==(Vector4 v1, Vector4 v2) => v1.Equals(v2);
+		public static bool operator !=(Vector4 v1, Vector4 v2) => !v1.Equals(v2);
+		#endregion
 
-        #region Standard Methods
-        public override readonly string ToString() => $"( X: {X}, Y: {Y}, Z: {Z} W: {W})";
-        #endregion
-    }
+		#region Standard Methods
+		public readonly bool Equals(Vector4 other) => X == other.X && Y == other.Y && Z == other.Z && W == other.W;
+		public override readonly string ToString() => $"( X: {X}, Y: {Y}, Z: {Z} W: {W})";
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode() ^ W.GetHashCode();
+		#endregion
+	}
 }
