57e4539ef41128f9c539259fa60b342cf5b20aa8
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-22 23:52:35 +1000

    Added a game header

---
+++ Tic-Tac-Toe/Game/Objects/Header.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleGameFramework.Graphics.StandardColorLibrary;
+using ConsoleGameFramework.Objects;
+using ConsoleGameFramework.Objects.Vectors;
+
+namespace Tic_Tac_Toe.Game.Objects
+{
+	internal readonly struct Header
+	{
+		public readonly CenterPoint _center;
+		public Header(int start, int end, int row) 
+		{
+			_center = new(start, end, row);
+		}
+
+		public void Update(GameState header, ColorPair colorPair)
+		{
+			colorPair.UseColor();
+			string temp = _headers[(int)header];
+			_center.WriteCenteredHorizontal(temp);
+			ColorPair.Default.UseColor();
+		}
+
+		public void Update(GameState header)
+		{
+			Update(header, ColorPair.Default);
+		}
+
+		public static readonly string[] _headers =
+		{
+			"Player X's Turn",
+			"Player Y's Turn",
+			"Player X Wins",
+			"Player Y Wins",
+			"Drawn Game"
+		};
+
+		public static int ArrayLength => _headers.Length;
+		public static int MaxLength => _headers.OrderByDescending(s => s.Length).First().Length;
+	}
+	internal enum GameState
+	{
+		TurnPlayer1,
+		TurnPlayer2,
+		WinPlayer1,
+		WinPlayer2,
+		GameDraw
+	}
+	
+}
