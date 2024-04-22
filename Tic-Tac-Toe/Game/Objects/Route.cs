bf7f92b48c3b1de6e4b7628fd55328bd2c2b61c5
Logicye <93573184+Logicye@users.noreply.github.com>
2024-04-22 23:55:59 +1000

    Added a route struct to handle route path finding and execution

---
+++ Tic-Tac-Toe/Game/Objects/Route.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleGameFramework.Objects.Vectors;
+
+namespace Tic_Tac_Toe.Game.Objects
+{
+	public struct Route
+	{
+		private readonly Vector2 currentPosition;
+		private readonly Vector2 targetPosition;
+		public List<Vector2> PathToTarget;
+
+		public Route(Vector2 current, Vector2 target) { 
+			currentPosition = current; 
+			targetPosition = target; 
+			PathToTarget = GeneratePath();
+		}
+
+		public List<Vector2> GeneratePath()
+		{
+			List<Vector2> result = new List<Vector2>();
+			return result;
+		}
+	}
+}
