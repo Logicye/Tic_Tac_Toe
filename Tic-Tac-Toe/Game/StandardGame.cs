e9c9807529051024ec563511616878d1d65b6aef
Logicye <93573184+Logicye@users.noreply.github.com>
2024-05-13 14:12:54 +1000

    Modified the StandardGame.cs file to utilise the new IUpdateEvents.cs interface registration system.

--- Tic-Tac-Toe/Game/StandardGame.cs
+++ Tic-Tac-Toe/Game/StandardGame.cs
@@ -2,51 +2,44 @@
 using Tic_Tac_Toe.Game.Objects;
 using Tic_Tac_Toe.Game.Players;
 
-namespace Tic_Tac_Toe.Game
+namespace Tic_Tac_Toe.Game;
+
+internal class StandardGame : IUpdateEvents
 {
-    internal class StandardGame : IUpdateEvents
-    {
-		private IPlayer _player1;
-		private IPlayer _player2;
-		private Board _board;
-		private GameState _state;
+	private IPlayer _player1;
+	private IPlayer _player2;
+	private Board _board;
+	private GameState _state;
 
-		public StandardGame(IPlayer player1, IPlayer player2)
-		{
-			_player1 = player1;
-			_player2 = player2;
-			_board = new Board();
-			_state = GameState.TurnPlayer1;
-			Initialize();
-		}
+	public StandardGame(IPlayer player1, IPlayer player2)
+	{
+		_player1 = player1;
+		_player2 = player2;
+		_board = new Board();
+		_state = GameState.TurnPlayer1;
+		Initialize();
+	}
 
-		public void Initialize()
-		{
-			Console.CursorVisible = false;
-			_board.Initialize();
-			_board.Update(_state, _player1, _player2);
-			Register();
-		}
+	public void Initialize()
+	{
+		Console.CursorVisible = false;
+		_board.Initialize();
+		_board.Update(_state, _player1, _player2);
+		IUpdateEvents.Register(this);
+	}
 
-		public void Register()
-		{
-			IPlayer.UpdateCall += OnUpdate;
-		}
+	public void Unregister()
+	{
+		IPlayer.UpdateCall -= OnUpdate;
+	}
 
-		public void Unregister()
-		{
-			IPlayer.UpdateCall -= OnUpdate;
-		}
+	public void OnUpdate()
+	{
+		_board.Update(_state, _player1, _player2);
+	}
 
-		public void OnUpdate()
-		{
-			_board.Update(_state, _player1, _player2);
-		}
-
-		public void GameOver() 
-		{
-			
-		}
-
+	public void GameOver() 
+	{
+		
 	}
 }
