2efbf811c5535443bc038c7350d1c6b7b0200b06
Logicye <93573184+Logicye@users.noreply.github.com>
2024-05-14 16:22:35 +1000

    Removed old IBot interface and relocated it under the Bots namespace with new reduced logic set, where the Bot abstract class creates the majority of the logic

---
+++ Tic-Tac-Toe/Game/Players/Bots/IBot.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleGameFramework.Objects.Vectors;
+using Tic_Tac_Toe.Game.Objects;
+
+namespace Tic_Tac_Toe.Game.Players.Bots;
+
+internal interface IBot
+{
+    public ComputerPlayer Player { get; }
+    public Vector2 Target { get; }
+    //Finds the next move to make based on the bots logic and sets the target to the new square
+    public void NextMove(Cell[,] gameBoard);
+    public void EmulateBotMove();
+}
